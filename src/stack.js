eb9b3631d74d83cbd5963e5b4c75cc7d538c3862
Nadzeya Vaskouskaya <nadiavaskovskaya@gmail.com>
2023-12-24 21:56:04 +0300

    feat: add full task

--- src/stack.js
+++ src/stack.js
@@ -2,30 +2,37 @@ const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
+ * Реализуйте стек с заданным интерфейсом через массив.
  *
  * @example
  * const stack = new Stack();
  *
  * stack.push(1); // adds the element to the stack
+ * добавляет элемент в стек
  * stack.peek(); // returns the peek, but doesn't delete it, returns 1
+ *  возвращает элемент, но не удаляет его, возвращает 1
  * stack.pop(); // returns the top element from stack and deletes it, returns 1
+ * возвращает верхний элемент из стека и удаляет его, возвращает 1
  * stack.pop(); // undefined
  *
  */
 class Stack {
+  constructor() {
+    this.arrStack = [];
+  }
+
   push(element) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    this.arrStack.push(element);
   }
 
   pop() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    return this.arrStack.length > 0 ? this.arrStack.pop() : undefined;
   }
 
   peek() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    return this.arrStack.length > 0
+      ? this.arrStack[this.arrStack.length - 1]
+      : undefined;
   }
 }
 
